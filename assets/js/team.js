b524684e856d6a4165617a5dbb4d8ec1c08db4d3
sameer0321 <nitinsainsrowa@gmail.com>
2023-09-28 12:36:19 +0530

    Ist page done

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,10 +1,10 @@
 let mobileView = document.querySelector(".mobileView");
 let menu = document.querySelector(".menu");
 let body = document.querySelector("body");
-let navlinks = document.querySelectorAll(".navlink");
 let span1 = document.querySelector(".span1");
 let span2 = document.querySelector(".span2");
 let span3 = document.querySelector(".span3");
+let dropdown = document.querySelectorAll(".dropdown-list");
 
 menu.addEventListener("click", function () {
     mobileView.classList.toggle("show");
@@ -13,19 +13,14 @@ menu.addEventListener("click", function () {
     span2.classList.toggle("rotate2");
     span3.classList.toggle("rotate3");
 })
-// navlinks.addEventListener("click", function () {
-//     mobileView.classList.toggle("show");
-//     body.classList.remove("overflow-hidden");
-//     span1.classList.toggle("rotate1");
-//     span2.classList.toggle("rotate2");
-//     span3.classList.toggle("rotate3");
-// })
-navlinks.forEach((navlinks))(
-    navlinks.addEventListener("click", function (){
-    mobileView.classList.toggle("show");
-    body.classList.remove("overflow-hidden");
-    span1.classList.toggle("rotate1");
-    span2.classList.toggle("rotate2");
-    span3.classList.toggle("rotate3");
-    }
-))
+
+
+dropdown.forEach((dropdown) => {
+    dropdown.addEventListener("click", function () {
+        mobileView.classList.toggle("show");
+        body.classList.remove("overflow-hidden");
+        span1.classList.toggle("rotate1");
+        span2.classList.toggle("rotate2");
+        span3.classList.toggle("rotate3");
+    });
+});
